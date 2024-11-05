892736e09159ec744c675639e77058dec9cf7603
J L <jl-@live.se>
2024-11-05 11:46:12 +0100

    Swagger

--- src/app.js
+++ src/app.js
@@ -1,7 +1,34 @@
-const express = require('express') //Importerat express
-const app = express()
-app.use(express.json()) //Hantera json data
+const express = require('express');
+const cors = require('cors')
+var favicon = require('serve-favicon');
+var path = require('path');
+const swaggerJsDoc = require("swagger-jsdoc");
+const swaggerUi = require("swagger-ui-express");
+
+
+const app = express();
+app.use(favicon(path.join(__dirname, '../public', 'favicon.ico')));
+app.use(cors());
+app.use(express.static('public'));
+
+const swaggerOptions = {
+    swaggerDefinition: {
+        info: {
+            version: "1.0.0",
+            title: "HTTP Classic API",
+            description: "API Information of http-classic-js",
+            contact: {
+                name: "user@example.com"
+            },
+            servers: ["http://localhost:3001"]
+        }
+    },
+    apis: ['./routes/*.js']
+};
+
+const swaggerDocs = swaggerJsDoc(swaggerOptions);
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocs));
 
 app.use('/api/user', require('./routes/user_routes.js'));
 
-module.exports = app //Exporterar app objektet så att den kan importeras i service.js
+module.exports = app;
