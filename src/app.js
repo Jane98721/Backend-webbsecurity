6eab24b6a9c6bae273c61a4c2762ca63722198c1
J L <jl-@live.se>
2024-11-14 12:42:34 +0100

    Startat en backend server

--- src/app.js
+++ src/app.js
@@ -1,34 +1,14 @@
 const express = require('express');
+const jwt = require ('jsonwebtoken')
 const cors = require('cors')
-var favicon = require('serve-favicon');
-var path = require('path');
-const swaggerJsDoc = require("swagger-jsdoc");
-const swaggerUi = require("swagger-ui-express");
-
+const bodyParser = require ('body-parser')
 
 const app = express();
-app.use(favicon(path.join(__dirname, '../public', 'favicon.ico')));
+
 app.use(cors());
-app.use(express.static('public'));
+app.use(bodyParser.json());
 
-const swaggerOptions = {
-    swaggerDefinition: {
-        info: {
-            version: "1.0.0",
-            title: "HTTP Classic API",
-            description: "API Information of http-classic-js",
-            contact: {
-                name: "user@example.com"
-            },
-            servers: ["http://localhost:3001"]
-        }
-    },
-    apis: ['./routes/*.js']
-};
+app.listen(4000, () => {
+    console.log('Server is running on port 4000')
+})
 
-const swaggerDocs = swaggerJsDoc(swaggerOptions);
-app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocs));
-
-app.use('/api/user', require('./routes/user_routes.js'));
-
-module.exports = app;
